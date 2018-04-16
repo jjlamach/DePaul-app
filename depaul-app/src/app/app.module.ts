c39b5d7ae60400466d8203323a25531251515dec
Julio Lama <lamajulio17@gmail.com>
2018-04-16 16:05:17 -0500

    s

--- depaul-app/src/app/app.module.ts
+++ depaul-app/src/app/app.module.ts
@@ -5,16 +5,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormComponentComponent } from './form-component/form-component.component';
 
+// the Forms module to use.
+import {FormsModule} from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponentComponent
+    FormComponentComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent] // <- render the app starting from this component
 })
+
+
+
 export class AppModule { }
--- depaul-app/src/app/form-component/form-component.component.ts
+++ depaul-app/src/app/form-component/form-component.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-form-component',
   templateUrl: './form-component.component.html',
   styleUrls: ['./form-component.component.css']
 })
-export class FormComponentComponent implements OnInit {
 
-  constructor() { }
+
+
+export class FormComponentComponent implements OnInit {
+  myForm: FormControl;
+
+  constructor() {
+    this.myForm = new FormControl(Validators.required);
+  }
 
   ngOnInit() {
+
+  }
+
+
+  onLogIn(form: any): void {
+    this.myForm = form;
+  }
+
+  checkFields(username, password) {
+    if (username.length <= 0 && password.length <= 0) {
+      console.log('Fields cannot be empty.');
+    } else if (username.length <= 0) {
+      console.log('Username field cannot be empty.');
+    } else if (password.length <= 0) {
+      console.log('Password field cannot be empty.');
+    }
   }
 
 }
