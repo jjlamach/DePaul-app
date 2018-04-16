63747dcb98baeab35e6822b54753dee5dce3a261
Julio Lama <lamajulio17@gmail.com>
2018-04-15 21:06:44 -0500

    form

--- depaul-app/src/app/app.module.ts
+++ depaul-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FormComponentComponent } from './form-component/form-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponentComponent
   ],
   imports: [
     BrowserModule
