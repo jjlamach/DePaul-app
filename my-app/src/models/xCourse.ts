74c5eb8068948c4d3817f01a9390d0d99c9691a9
mikostul <38091447+mikostul@users.noreply.github.com>
2018-05-14 11:37:13 -0400

    added xcourses so works

---
+++ my-app/src/models/xCourse.ts
@@ -0,0 +1,10 @@
+export class xCourse {
+ COURSE_ID: String;
+ OFFERED_FALL: String;
+ OFFERED_WINTER: String;
+ OFFERED_SPRING: String;
+ OFFERED_SUMMER: String;
+ PREREQS: String;
+ ONLINE: String;
+ constructor(values: Object = {} ) {Object.assign(this, values);}
+}
