2bc8579227450143095cedb9c14f9a38bce836d6
Julio L <lamajulio17@gmail.com>
2018-05-19 16:30:46 -0500

    Users or faculty cannot be added more than once.

--- my-app/src/app/aremove-user/aremove-user.component.ts
+++ my-app/src/app/aremove-user/aremove-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Student} from "../../models/Student";
+import {User} from "../../models/User";
+import {DataService} from "../Data.service";
 
 @Component({
   selector: 'app-aremove-user',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aremove-user.component.css']
 })
 export class AremoveUserComponent implements OnInit {
+  private students: Student[] = [];
 
-  constructor() { }
+
+  constructor(private service: DataService) { }
 
   ngOnInit() {
+    this.getStudents();
+  }
+
+
+  /**
+   * Get all the students.
+   */
+  getStudents() {
+    this.service.getStudents().subscribe((stdnts: Student[]) => {
+      this.students = stdnts;
+      console.log(this.students);
+      this.students.forEach((x) => console.log(x));
+    });
+  }
+
+  /**
+   *
+   * @param {Student} student
+   * @returns {Subscription}
+   */
+  delete(student: Student) {
+    this.service.deleteStudent(student.depaulID).subscribe(res =>{
+      console.log('Deleted');
+    }, (error) =>{
+      console.log(error);
+    });
+
   }
 
 }
